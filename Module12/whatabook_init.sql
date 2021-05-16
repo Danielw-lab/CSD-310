ac8f12c30d832bc46ba04b855b856c3f79403229
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2021-05-15 19:59:55 -0500

    Module12 assignment

---
+++ Module12/whatabook_init.sql
@@ -0,0 +1,125 @@
+-- drop test user if exists 
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
+
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
+-- drop contstraints if they exist
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+
+-- drop tables if they exist
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
+
+/*
+    Create table(s)
+*/
+CREATE TABLE store (
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    store_time  VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
+);
+
+CREATE TABLE book (
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
+);
+
+CREATE TABLE user (
+    user_id         INT         NOT NULL    AUTO_INCREMENT,
+    first_name      VARCHAR(75) NOT NULL,
+    last_name       VARCHAR(75) NOT NULL,
+    PRIMARY KEY(user_id) 
+);
+
+CREATE TABLE wishlist (
+    wishlist_id     INT         NOT NULL    AUTO_INCREMENT,
+    user_id         INT         NOT NULL,
+    book_id         INT         NOT NULL,
+    PRIMARY KEY (wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY (book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_Id)
+);
+
+/*
+    insert store record 
+*/
+INSERT INTO store(locale,store_time)
+    VALUES('123 Street , Omaha, NE 68116','Mon-Fri, 8:00 am-5:00 pm');
+
+/*
+    insert book records 
+*/
+INSERT INTO book(book_name, author, details)
+    VALUES('Ocean Prey', 'John Sandford', 'Remarkable new novel from #1 New York Times');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Hill We Climb', 'Amanda Gorman', 'Foreword by Oprah Winfrey');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Devil Hand', 'Jack Carr','New York Times bestselling author');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Four Winds', 'Kristin Hannah');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Midnight Library', 'Matt Haig');
+
+INSERT INTO book(book_name, author)
+    VALUES("The Red Book", 'James Patterson');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Good Sister', 'Sally Hepworth');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Lion, the Witch, and the Wardrobe', 'C.S. Lewis');
+
+INSERT INTO book(book_name, author)
+    VALUES('The Catcher and the Rye', 'J.D. Salinger');
+
+/*
+    insert user
+*/ 
+INSERT INTO user(first_name, last_name) 
+    VALUES('Michel', 'Robert');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Dav', 'Rrcard');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Tome', 'Bab');
+
+/*
+    insert wishlist records 
+*/
+INSERT INTO wishlist(user_id, book_id) 
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Michel'), 
+        (SELECT book_id FROM book WHERE book_name = 'The Four Winds')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Dav'),
+        (SELECT book_id FROM book WHERE book_name = 'The Hill We Climb')
+    );
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Tome'),
+        (SELECT book_id FROM book WHERE book_name = 'Ocean Prey')
+    );
